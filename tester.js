b27aa394864b5456d32688cfa3c1c43c20c293b8
chipbaber <chipbaber@yahoo.com>
2019-05-08 20:32:19 +0000

    tester.js got reduce working

--- tester.js
+++ tester.js
@@ -1,16 +1,29 @@
 console.log("Testing fetch/reduce");
 const fetch = require('node-fetch');
+/*
+var practice1 =  fetch('https://www.nascar.com/cacher/2019/1/4783/entryList.json');
+practice1.then(data => data.json()).then(json => console.log(json)).catch(err=>console.log(err));
+*/
+//or async
 
-//var practice1 =  fetch('https://www.nascar.com/cacher/2019/1/4783/entryList.json');
-//practice1.then(data => data.json()).then(json => console.log(json)).catch(err=>console.log(err));
-
-//or
 var url = "https://www.nascar.com/cacher/2019/1/4783/entryList.json";
 var getData = async url => {
   try {
     var response = await fetch(url);
     var json = await response.json();
+    var temp ={};
     console.log(json);
+    /*reducing*/
+    var json = json.reduce(function(pv, cv) {
+       pv[cv.vehicle_number]={};
+       pv[cv.vehicle_number].driver_name = cv.driver_name;
+       
+        return pv;
+    }, {});
+
+ console.log(json);
+
+
   } catch (error) {
     console.log(error);
   }
